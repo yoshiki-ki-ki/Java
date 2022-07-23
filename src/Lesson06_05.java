f60299abd4d9774d0b8dcecf5201985ddb3ab92e
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-23 22:15:21 +0900

    [Lesson06_05]

---
+++ src/Lesson06_05.java
@@ -0,0 +1,10 @@
+public class Lesson06_05 {
+  public static void main(String[] args){
+    int VAR = 1;
+    int Var = 2;
+
+    System.out.println(VAR);
+    System.out.println(Var);
+  }
+}
+// 変数は大文字と小文字を区別している。
