3f698bc7ca37859dbe343ea9a60b8c7004575f70
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-23 22:07:33 +0900

    [Lesson06_03]

---
+++ src/Lesson06_03.java
@@ -0,0 +1,14 @@
+public class Lesson06_03 {
+  public static void main(String[] args) {
+    int var = 1;
+    int var01 = 2;
+    int var_01 = 3;
+    int var$01 = 4;
+
+    System.out.println(var);
+    System.out.println(var01);
+    System.out.println(var_01);
+    System.out.println(var$01);
+  }
+}
+// 変数名にはアルファベットの後に数字、アンダーバー、ドルマークを使うことができる。
