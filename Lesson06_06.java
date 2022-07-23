e445d8b930e700c77413514da5f93d1bdeec00ed
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-23 22:19:35 +0900

    [Lesson06_06]

---
+++ Lesson06_06.java
@@ -0,0 +1,11 @@
+public class Lesson06_06 {
+  // public static void main(String[] args){
+  //   int return = 1;
+  //   int class = 1;
+
+  //   System.out.plintln(return);
+  //   System.out.println(class);
+  // }
+}
+// 変数名に予約語は利用できない。
+// 「return」「class」「for」「while」などのすでに役割の決まっている単語。
