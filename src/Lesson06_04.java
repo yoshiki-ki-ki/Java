ad719b1d08a4dc29af23ed3ea88ba065c9d9c021
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-23 22:12:46 +0900

    [Lesson06_04]

---
+++ src/Lesson06_04.java
@@ -0,0 +1,10 @@
+public class Lesson06_04 {
+  // public static void main(String[] args){
+  //   int var-01 = 1;
+  //   int 01var = 1;
+
+  //   System.out.println(var-01);
+  // }
+}
+// 変数に「_」「$」以外の記号は使えない。
+// また、数字を先に入れることもできない。
