fea3452cadcb12862d85983aa72a9364eafd3d03
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-25 23:35:23 +0900

    [Lesson07_01]コメント追記

--- src/Lesson07_01.java
+++ src/Lesson07_01.java
@@ -1,12 +1,12 @@
 public class Lesson07_01 {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     // -100～100まで
     byte var01 = 1;
     // -3万～3万まで
     short var02 = 12345;
     // -20億～20憶まで
     int var03 = 1234567890;
-    // 「京」の桁以上
+    // 「京」の桁以上。最後に大文字Lを記載する必要あり。
     long var04 = 123456789000L;
 
     System.out.println(var01);
