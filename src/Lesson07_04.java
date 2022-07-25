1e24a5b3554c9051a80d4ca68c84c55c0ba61007
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-25 23:48:02 +0900

    [Lesson07_04]

---
+++ src/Lesson07_04.java
@@ -0,0 +1,15 @@
+public class Lesson07_04 {
+  public static void main(String[] args){
+
+    int var_a = 10;
+    int var_b = 1;
+
+    // ブール型（ブーリアン型）trueとfalseを持つ型。
+    boolean var_bool;
+
+    // ↓trueがかえってくる。
+    var_bool = (var_a > var_b);
+
+    System.out.println(var_bool);
+  }
+}
