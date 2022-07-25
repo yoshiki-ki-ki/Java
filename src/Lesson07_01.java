7ab7ffc85c035be68348910513d30678cd939f01
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-25 23:33:35 +0900

    [Lesson07_01]

---
+++ src/Lesson07_01.java
@@ -0,0 +1,17 @@
+public class Lesson07_01 {
+  public static void main(String[] args){
+    // -100～100まで
+    byte var01 = 1;
+    // -3万～3万まで
+    short var02 = 12345;
+    // -20億～20憶まで
+    int var03 = 1234567890;
+    // 「京」の桁以上
+    long var04 = 123456789000L;
+
+    System.out.println(var01);
+    System.out.println(var02);
+    System.out.println(var03);
+    System.out.println(var04);
+  }
+}
