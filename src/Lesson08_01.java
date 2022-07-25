3b93860f46ca70bfd2143a234ca01c31e2d5a29f
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-26 00:58:02 +0900

    [Lesson08_01]

---
+++ src/Lesson08_01.java
@@ -0,0 +1,16 @@
+// https://youtu.be/clEol_NaCEs
+
+public class Lesson08_01 {
+  public static void main(String[] args){
+    String[] arr;
+    arr = new String[3];
+
+    arr[0]="sato";
+    arr[1]="suzuki";
+    arr[2]="takahasi";
+
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+  }
+}
