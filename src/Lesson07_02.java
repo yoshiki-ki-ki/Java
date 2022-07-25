672ce3df16af9b5a96d00fcd21467e4144d595d5
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-25 23:38:39 +0900

    [Lesson07_02]

---
+++ src/Lesson07_02.java
@@ -0,0 +1,11 @@
+public class Lesson07_02 {
+  public static void main(String[] args){
+    // 小数点以下の桁数が多く扱える。
+    double var01 = 1.23456789;
+    // 小数点以下の桁数が「double」より少なく、最後に「F」を記載する必要あり。
+    float var02 = 1.234F;
+
+    System.out.println(var01);
+    System.out.println(var02);
+  }
+}
