44693ac2001d268d76843d45a5b732a0c4492968
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-26 01:03:47 +0900

    [Lesson08_02]

---
+++ src/Lesson08_02.java
@@ -0,0 +1,9 @@
+public class Lesson08_02 {
+  public static void main(String[] args){
+    String[] arr = {"sato","suzuki","takahasi"};
+
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+  }
+}
