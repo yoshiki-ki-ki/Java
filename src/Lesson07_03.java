4ac8042dc0a457707007ced1d6494faa10f8de90
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-07-25 23:42:12 +0900

    [Lesson07_03]

---
+++ src/Lesson07_03.java
@@ -0,0 +1,11 @@
+public class Lesson07_03 {
+  public static void main(String[] args){
+    // 文字列1文字とシングルクォーテーションを利用。
+    char var01 = 'c';
+    // 2文字以上の文字列とダブルクォーテーションを利用。
+    String var02 = "hello,World!";
+
+    System.out.println(var01);
+    System.out.println(var02);
+  }
+}
