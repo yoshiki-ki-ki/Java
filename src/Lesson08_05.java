61b897fb5c84770511c4593ef6867f4bf6441a96
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-08-08 21:28:49 +0900

    [Lesson08_05]

---
+++ src/Lesson08_05.java
@@ -0,0 +1,12 @@
+public class Lesson08_05 {
+  public static void main(String[] args){
+    String[][] arr = {{"sato","suzuki"},{"takahashi","tanaka"}};
+
+    System.out.println(arr[0][0]);
+    System.out.println(arr[0][1]);
+    System.out.println(arr[1][0]);
+    System.out.println(arr[1][1]);
+  }
+}
+
+// Lesson08_04のコードを省略したもの
