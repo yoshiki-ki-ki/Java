544f8b774c21d26012967aa5c0dd68426917c913
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-08-11 17:26:27 +0900

    [Lesson10]

---
+++ src/Lesson10_04.java
@@ -0,0 +1,11 @@
+public class Lesson10_04 {
+  public static void main(String[] args) {
+    int age = 22;
+
+    if (age >= 20) {
+      System.out.println("adult");
+    } else {
+      System.out.println("child");
+    }
+  }
+}
