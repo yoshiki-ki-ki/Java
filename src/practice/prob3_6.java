61af36112a42c8e543e9e0be5bb26d9682290eef
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-08-12 19:35:25 +0900

    [prob3_6]

---
+++ src/practice/prob3_6.java
@@ -0,0 +1,10 @@
+package practice;
+public class prob3_6 {
+  public static void main(String[] args) {
+    int num = (int)(Math.random()*6)+1;    //  1から6までの乱数を発生させる
+    System.out.println("数値 : " + num);
+    if(num >= 3){
+      System.out.println("3以上です");
+    }
+}
+}
