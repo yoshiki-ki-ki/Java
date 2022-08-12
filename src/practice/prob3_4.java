1692c4e810643afc3059a7c46e2ac7949420a1d8
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-08-12 19:28:30 +0900

    [prob3_4]

---
+++ src/practice/prob3_4.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class prob3_4 {
+  public static void main(String[] args) {
+    int num = (int)(Math.random()*100)+1;
+    System.out.println(num);
+    if(num <= 10 || num >= 90){
+      System.out.println("10以下か、90以上の値です");
+    }
+  }
+}
