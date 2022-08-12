a505740071d074c143d1cce2fa05faa95c98f6f5
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-08-12 19:13:54 +0900

    [prob3_3]

---
+++ src/practice/prob3_3.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class prob3_3 {
+  public static void main(String[] args) {
+    int num = (int)(Math.random()*100)+1;
+    System.out.println(num);
+    if(num < 50){
+      System.out.println("50未満です");
+    }
+  }
+}
