a90f1d0656cf9c4ae1c1898bf458e027da5e60e8
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-08-12 19:08:02 +0900

    [prob3_2]

---
+++ src/practice/prob3_2.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class prob3_2 {
+  public static void main(String[] args) {
+    int num = (int)(Math.random()*10)+1;
+    System.out.println(num);
+    if(num == 1){
+
+    } else{
+      System.out.println("1ではありません");
+    }
+  }
+}
